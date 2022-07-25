c2f809537e77fa10bb1874739c5d784e00b48d92
SFxImpulse <braverman.david.business@gmail.com>
2022-07-25 19:15:17 +0000

    add first queries

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,9 +1,13 @@
+-- Cohorts Table
+
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   start_date DATE,
   end_date DATE
 );
+
+-- Student Table
 
 CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
